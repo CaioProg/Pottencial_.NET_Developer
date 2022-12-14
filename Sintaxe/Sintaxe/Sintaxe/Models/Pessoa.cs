b5e914eaac4940fb2df527d868c297b2de2fafce
CaioProg <caioprog18@gmail.com>
2022-12-14 18:15:34 -0300

    Tipos de dados

--- Sintaxe/Sintaxe/Sintaxe/Models/Pessoa.cs
+++ Sintaxe/Sintaxe/Sintaxe/Models/Pessoa.cs
@@ -10,11 +10,14 @@ namespace Sintaxe.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        public string NomeRepresentanteLegalDaPessoaFisica { get; set; }
 
         public void Apresentar()
         {
 
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+            //Console.WriteLine($"Olá, meu nome é {Nome},\n e tenho {Idade} anos");
+
         }
     }
 }
--- Sintaxe/Sintaxe/Sintaxe/Program.cs
+++ Sintaxe/Sintaxe/Sintaxe/Program.cs
@@ -1,10 +1,43 @@
 ﻿using Sintaxe.Models;
 
-Pessoa pessoa1 = new Pessoa();
-
-pessoa1.Nome = "Caio";
-pessoa1.Idade = 20;
-
-pessoa1.Apresentar();
 
 
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+
+
+
+
+
+
+//string apresentacao = "Olá, seja bem vindo";
+
+//int quantidade = 1;
+//Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+//quantidade = 10;
+//Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+//double altura = 1.82;
+
+//decimal preco = 1.80M;
+
+//bool condicao = true;
+
+//Console.WriteLine(apresentacao);
+//Console.WriteLine("Valor da variável quantidade: " + quantidade);
+//Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+//Console.WriteLine("Valor da variável preco: " + preco);
+//Console.WriteLine("Valor da variável condicao: " + condicao);
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Caio";
+//pessoa1.Idade = 20;
+
+//pessoa1.Apresentar();
+
+//Pessoa pessoa_FisicaRepresentacaoLegal =  new Pessoa();
