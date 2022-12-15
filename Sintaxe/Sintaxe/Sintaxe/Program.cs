dc4344bc1e0080b43e1135537aebb9c8b8309ecb
CaioProg <caioprog18@gmail.com>
2022-12-14 21:31:12 -0300

    Cast Implícito

--- Sintaxe/Sintaxe/Sintaxe/Program.cs
+++ Sintaxe/Sintaxe/Sintaxe/Program.cs
@@ -3,10 +3,30 @@
 
 
 
-int inteiro = 5;
-string a = inteiro.ToString();
 
-Console.WriteLine(a);
+//int a = 5;
+//double b = a;
+
+//long a = long.MaxValue;
+//int b = Convert.ToInt32(a);
+
+int a = int.MaxValue;
+long b = a;
+
+Console.WriteLine(b);
+
+
+
+
+
+
+
+//int inteiro = 5;
+//string a = inteiro.ToString();
+
+
+
+//Console.WriteLine(a);
 
 
 
