e222a43cd87c0b45bc1b8db436134c9d0c9d3228
CaioProg <caioprog18@gmail.com>
2022-12-16 18:02:02 -0300

    Interrompendo o fluxo de execução

--- Sintaxe/Sintaxe/Sintaxe/Program.cs
+++ Sintaxe/Sintaxe/Sintaxe/Program.cs
@@ -3,14 +3,18 @@
 
 
 int numero = 5;
-int contador= 0;
+int contador = 1;
 
 while (contador <= 10)
 {
-    Console.WriteLine($"{contador + 1}° Execução: {numero} x {contador} = {numero * contador}");
+    Console.WriteLine($"{contador}° Execução: {numero} x {contador} = {numero * contador}");
     contador++;
-}
 
+    if (contador == 5)
+    {
+        break;
+    }
+}
 
 
 
