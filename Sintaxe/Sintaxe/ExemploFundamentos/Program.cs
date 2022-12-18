399d2df07816d3b3a5edb2540682d687d931757a
CaioProg <caioprog18@gmail.com>
2022-12-18 11:07:33 -0300

    Percorrendo um Array com foreach

--- Sintaxe/Sintaxe/ExemploFundamentos/Program.cs
+++ Sintaxe/Sintaxe/ExemploFundamentos/Program.cs
@@ -6,7 +6,21 @@ arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 
+
+
+Console.WriteLine("Percorrendo o Array com o FOR");
+
 for (int contador = 0; contador < arrayInteiros.Length; contador++)
 {
    Console.WriteLine($"Posição N {contador} - {arrayInteiros[contador]}");
+}
+
+
+Console.WriteLine("Percorrendo o Array com o FOREACH");
+
+int contadorForeach = 0;
+foreach(int valor in arrayInteiros)
+{
+      Console.WriteLine($"Posição N {contadorForeach} - {valor}");
+      contadorForeach++;
 }
