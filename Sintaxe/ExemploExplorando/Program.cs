8b2483619bad517a946ae52c1a5542ed04239df4
CaioProg <caioprog18@gmail.com>
2022-12-19 14:23:10 -0300

    Criando um método

--- Sintaxe/ExemploExplorando/Program.cs
+++ Sintaxe/ExemploExplorando/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Caio";
+p1.Idade = 20;
+p1.Apresentar();
