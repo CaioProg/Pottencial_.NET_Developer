1a4216deaa82950da4a73107e9aa0d6971e10593
CaioProg <caioprog18@gmail.com>
2022-12-21 18:22:57 -0300

    Pilha na prática

--- Sintaxe/ExemploExplorando/Program.cs
+++ Sintaxe/ExemploExplorando/Program.cs
@@ -1,27 +1,62 @@
 ﻿using ExemploExplorando.Models;
 using System.Globalization;
 
+Stack<int> pilha = new Stack<int>();
 
-Queue<int> fila = new Queue<int>();
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
 
-fila.Enqueue(2);
-fila.Enqueue(4);
-fila.Enqueue(6);
-fila.Enqueue(8);
-
-
-foreach(int item in fila)
+foreach(int item in pilha)
 {
     Console.WriteLine(item);
 }
 
-Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
-fila.Enqueue(10);
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
 
-foreach(int item in fila)
+pilha.Push(20);
+
+foreach(int item in pilha)
 {
     Console.WriteLine(item);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
 
 
 
