a1eb73de67eda13a369db8665d5b8dc1bb9e5613
CaioProg <caioprog18@gmail.com>
2022-12-27 13:58:43 -0300

    Escrevendo um arquivo JSON

--- Sintaxe/ExemploExplorando/Program.cs
+++ Sintaxe/ExemploExplorando/Program.cs
@@ -6,6 +6,8 @@ Venda v1 = new Venda(1, "Material de escritorio", 25.00M);
 
 string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
 
+File.WriteAllText("Arquivos/vendas.json", serializado);
+
 Console.WriteLine(serializado);
 
 
