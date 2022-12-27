f6423627c13fc809f297f101dbbd776fc0e29e2d
CaioProg <caioprog18@gmail.com>
2022-12-27 14:25:55 -0300

    Deserializando um objeto

--- Sintaxe/ExemploExplorando/Models/Venda.cs
+++ Sintaxe/ExemploExplorando/Models/Venda.cs
@@ -7,17 +7,9 @@ namespace ExemploExplorando.Models
 {
     public class Venda
     {
-        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
-        {
-            Id= id;
-            Produto = produto;
-            Preco = preco;
-            DataVenda = dataVenda;
-        }
-
         public int Id { get; set; }
         public string Produto { get; set; }
         public decimal Preco { get; set; }
-        public DateTime DataVenda {get; set; }
+        public DateTime DataVenda { get; set; }
     }
 }
--- Sintaxe/ExemploExplorando/Program.cs
+++ Sintaxe/ExemploExplorando/Program.cs
@@ -2,21 +2,52 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
-DateTime dataAtual = DateTime.Now;
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
-List<Venda> listaVendas = new List<Venda>();
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
-Venda v1 = new Venda(1, "Material de escritorio", 25.00M, dataAtual);
-Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+foreach (Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
 
-listaVendas.Add(v1);
-listaVendas.Add(v2);
 
-string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
-File.WriteAllText("Arquivos/vendas.json", serializado);
 
-Console.WriteLine(serializado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now;
+
+// List<Venda> listaVendas = new List<Venda>();
+
+// Venda v1 = new Venda(1, "Material de escritorio", 25.00M, dataAtual);
+// Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+
+// listaVendas.Add(v1);
+// listaVendas.Add(v2);
+
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+
+// Console.WriteLine(serializado);
 
 
 
