7a9f375c9968a0d4bbe9372233f12274fbe5a530
CaioProg <caioprog18@gmail.com>
2022-12-27 14:51:21 -0300

    Tipos nulos na prática

--- Sintaxe/ExemploExplorando/Program.cs
+++ Sintaxe/ExemploExplorando/Program.cs
@@ -2,14 +2,46 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
-string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
-List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+bool? desejaReceberEmail = true;
 
-foreach (Venda venda in listaVenda)
+
+if (desejaReceberEmail.HasValue && desejaReceberEmail.Value)
 {
-    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+    Console.WriteLine("O usuário optou por receber e-mail.");
 }
+else
+{
+    Console.WriteLine("O usuário não respondeu ou optou por não receber e-mail.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+
+// List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+// foreach (Venda venda in listaVenda)
+// {
+//     Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+// }
 
 
 
