cba031e9162cd80a26f82f9432f0157e36066d60
CaioProg <caioprog18@gmail.com>
2022-12-27 14:05:24 -0300

    Serializando uma coleção

--- Sintaxe/ExemploExplorando/Program.cs
+++ Sintaxe/ExemploExplorando/Program.cs
@@ -2,9 +2,15 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
-Venda v1 = new Venda(1, "Material de escritorio", 25.00M);
+List<Venda> listaVendas = new List<Venda>();
 
-string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+Venda v1 = new Venda(1, "Material de escritorio", 25.00M);
+Venda v2 = new Venda(2, "Licença de Software", 110.00M);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
 File.WriteAllText("Arquivos/vendas.json", serializado);
 
