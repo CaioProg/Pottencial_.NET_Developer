6262132db5886565abe621bd830f9785d65b9b5b
CaioProg <caioprog18@gmail.com>
2022-12-27 14:13:38 -0300

    DateTime em JSON

--- Sintaxe/ExemploExplorando/Program.cs
+++ Sintaxe/ExemploExplorando/Program.cs
@@ -2,10 +2,12 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
+DateTime dataAtual = DateTime.Now;
+
 List<Venda> listaVendas = new List<Venda>();
 
-Venda v1 = new Venda(1, "Material de escritorio", 25.00M);
-Venda v2 = new Venda(2, "Licença de Software", 110.00M);
+Venda v1 = new Venda(1, "Material de escritorio", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
 
 listaVendas.Add(v1);
 listaVendas.Add(v2);
