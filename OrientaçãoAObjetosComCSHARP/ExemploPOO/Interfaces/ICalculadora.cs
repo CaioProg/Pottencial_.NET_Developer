6fbc5dde236587c5a3c5e44fe228f7286a8e6973
CaioProg <caioprog18@gmail.com>
2023-01-10 10:52:38 -0300

    Método padrão na interface

--- OrientaçãoAObjetosComCSHARP/ExemploPOO/Interfaces/ICalculadora.cs
+++ OrientaçãoAObjetosComCSHARP/ExemploPOO/Interfaces/ICalculadora.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 namespace ExemploPOO.Interfaces {
     public interface ICalculadora
     {
+        // Métodos que não tem corpo são obritario a implementação
         int Somar(int num1, int num2);
         int Subtrair(int num1, int num2);
         int Multiplicar(int num1, int num2);
-        int Dividir(int num1, int num2);
+        
+        // Métodos que tem corpo são opcionail na implementação
+        int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
     }
 }
--- OrientaçãoAObjetosComCSHARP/ExemploPOO/Models/Calculadora.cs
+++ OrientaçãoAObjetosComCSHARP/ExemploPOO/Models/Calculadora.cs
@@ -8,11 +8,6 @@ namespace ExemploPOO.Models
 {
     public class Calculadora : ICalculadora
     {
-        public int Dividir(int num1, int num2)
-        {
-            return num1 / num2;
-        }
-
         public int Multiplicar(int num1, int num2)
         {
             return num1 * num2;
