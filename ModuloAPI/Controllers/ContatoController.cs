85f5fd007d97a4d789bc9b3e8af239e78417fe73
CaioProg <caioprog18@gmail.com>
2023-01-19 11:04:56 -0300

    Criando a controller e o endpoint de Create

---
+++ ModuloAPI/Controllers/ContatoController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModuloAPI.Context;
+using ModuloAPI.Entities;
+
+namespace ModuloAPI.Controllers
+{    
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+    }
+}
