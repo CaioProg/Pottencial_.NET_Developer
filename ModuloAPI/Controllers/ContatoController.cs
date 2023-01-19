5227cc8ee88eb8ad0f13d9e36d668b24b8e9546f
CaioProg <caioprog18@gmail.com>
2023-01-19 11:08:59 -0300

    Criando o endpoint obter por ID

--- ModuloAPI/Controllers/ContatoController.cs
+++ ModuloAPI/Controllers/ContatoController.cs
@@ -26,5 +26,16 @@ namespace ModuloAPI.Controllers
             _context.SaveChanges();
             return Ok(contato);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            
+            if(contato == null)        
+                return NotFound();
+
+            return Ok(contato);
+        }
     }
 }
