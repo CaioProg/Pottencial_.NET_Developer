8dce659970c201ab73a7d8523d8b03de2f761005
CaioProg <caioprog18@gmail.com>
2023-01-20 16:37:10 -0300

    Configurando o Entity Framework

--- ProjetoMVC/Program.cs
+++ ProjetoMVC/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
