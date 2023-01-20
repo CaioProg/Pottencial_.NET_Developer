67ac9ca3f48eb8e4ea1038985f9b27ae09153f9e
CaioProg <caioprog18@gmail.com>
2023-01-20 17:06:05 -0300

    Configurando o método na controller

--- ProjetoMVC/Controllers/ContatoController.cs
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
 
 namespace ProjetoMVC.Controllers
 {
     public class ContatoController : Controller
-    {
-        public IActionResult Index()
+    {   
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
         {
-            return View();
+            _context = context;
+        }
+        public IActionResult Index()
+        {   
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
