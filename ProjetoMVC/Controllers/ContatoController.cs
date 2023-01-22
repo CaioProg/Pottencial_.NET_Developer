a385f109e56afc7fe8775595d9e47d120f7ea3c0
CaioProg <caioprog18@gmail.com>
2023-01-22 13:42:57 -0300

    Criando o POST do editar

--- ProjetoMVC/Controllers/ContatoController.cs
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -47,5 +47,20 @@ namespace ProjetoMVC.Controllers
 
             return View(contato);
         }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
