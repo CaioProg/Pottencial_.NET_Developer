ff069524e2066d25932edf9c547b12e04da8203a
CaioProg <caioprog18@gmail.com>
2023-01-22 14:00:43 -0300

    Criando a página de detalhes

--- ProjetoMVC/Controllers/ContatoController.cs
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -62,5 +62,15 @@ namespace ProjetoMVC.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if(contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
     }
 }
