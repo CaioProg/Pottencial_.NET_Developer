0e9618f0a1f7e89acd486d29d98ca71f69442c65
CaioProg <caioprog18@gmail.com>
2023-01-22 14:33:10 -0300

    Criando o POST do Deletar

--- ProjetoMVC/Controllers/ContatoController.cs
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -82,5 +82,16 @@ namespace ProjetoMVC.Controllers
 
             return View(contato);
         }
+
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
